25cb395592e185c0f9c9819f67e1451f8e240fca
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-06-08 13:07:24 -0700

    add code

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add your JS here.
+/document.getElementById("demo").addEventListener("keypress", myFunction);
+
+function myTodolist() {
+  document.getElementById("myTodolist").style.backgroundColor = "13";
+}
+ function Sum (arr){
+   let
+   for( let i = 0; i < 13; j++ ) {
+    console.log(j);
+ }
+ <div id = "one" > one < /div>
+var d1 = document.getElementById('one');
+d1.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
+
+console.console.log("Organzie Kitchen");
+console.console.log(["Hit the gym", "Pay Bills", "Meet Debbie", "Buy Eggs", "Do homework for the Iron Yard", "Organize Kitchen"]);
+console.console.console.log("Pay Bills");
+console.console.console.log("Meet Debbie");
+console.console.log("Buy Eggs");
+console.console.log("Do homework for the Iron Yard");/ Add your JS here.
